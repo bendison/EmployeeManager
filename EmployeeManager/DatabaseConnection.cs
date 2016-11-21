087ad90c7d9e37f8edcd95a29e72b33821deab8f
DiscoveriesMC <discoveriesmc@hotmail.com>
2016-11-21 11:24:40 -0500

    Initial commit

---
+++ EmployeeManager/DatabaseConnection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EmployeeManager
+{
+    class DatabaseConnection
+    {
+        private MySqlConnection db;
+        private string connectionString;
+        private Auth auth;
+
+        public DatabaseConnection()
+        {
+            RetrieveConnectionInformation();
+                       
+            connectionString = $"server={auth.server};uid={auth.username};" + $"pwd={auth.password};database={auth.database};";
+            try
+            {
+                db = new MySqlConnection(connectionString);
+                db.Open();
+                MessageBox.Show("Connection established successfully.");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(
+                    $"Unable to establish a connection to the employee database. Please contact an administrator.\nError: {ex.Message}");
+            }
+        }
+
+        private void RetrieveConnectionInformation()
+        {
+            try
+            {
+                auth = JsonConvert.DeserializeObject<Auth>(File.ReadAllText(@Path.GetDirectoryName(Application.ExecutablePath) + "\\config.json"));
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        protected class Auth
+        {
+            public string server { get; set; }
+            public string database { get; set; }
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
+    }
+}
---
+++ EmployeeManager/EmployeeManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace EmployeeManager
+{
+    public partial class EmployeeManager : Form
+    {
+
+        private DatabaseConnection db = new DatabaseConnection();
+
+        public EmployeeManager()
+        {
+            InitializeComponent();
+            MessageBox.Show(System.IO.Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath) + "\\config.json");
+        }
+    }
+}
