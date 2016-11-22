34cf17a89401c6426e09c654747e2ab5048b4021
DiscoveriesMC <discoveriesmc@hotmail.com>
2016-11-22 00:25:36 -0500

    Removed test comments

--- EmployeeManager/EmployeeManager.cs
+++ EmployeeManager/EmployeeManager.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-//test123
+
 namespace EmployeeManager
 {
     public partial class EmployeeManager : Form
