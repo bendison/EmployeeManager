b98675a645d99c5e0b7ab99304ac7d4925c5c05a
DiscoveriesMC <discoveriesmc@hotmail.com>
2016-12-02 15:40:01 -0500

    Remove Position enum, add support for positions table

--- EmployeeManager/Position.cs
+++ EmployeeManager/Position.cs
@@ -1,36 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace EmployeeManager
 {
     class Position
     {
-        public static readonly Position Trainee = new Position("Trainee", 0);
-        public static readonly Position Janitor = new Position("Janitor", 1);
-        public static readonly Position It = new Position("It", 2);
-        public static readonly Position Maintenance = new Position("Maintenance", 3);
-        public static readonly Position Chemist = new Position("Chemist", 4);
-        public static readonly Position LabTech = new Position("LabTech", 5);
-        public static readonly Position Hr = new Position("Hr", 6);
-        public static readonly Position Supervisor = new Position("Supervisor", 7);
-        public static readonly Position PlantOfficer = new Position("PlantOfficer", 8);
+        public int ID { get; set; }
+        public string Title { get; set; }
 
-        public static IEnumerable<Position> Values = new List<Position>() { Trainee, Janitor, It, Maintenance, Chemist, LabTech, Hr, Supervisor, PlantOfficer};
-
-        private readonly string name;
-        private readonly int id;
-        private readonly string dBColumn;
-
-        Position(string name, int id)
+        public Position(int id, string title)
         {
-            this.name = name;
-            this.id = id;
+            ID = id;
+            Title = title;
         }
-
-        public string Name { get { return name; } }
-        public int Id { get { return id; } }
     }
 }
